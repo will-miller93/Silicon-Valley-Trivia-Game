9fd87b201943fec8f181d884b4ec0fbb492aa721
Will Miller <willmiller@Wills-MBP.attlocal.net>
2018-03-22 22:45:47 -0400

    initial changes

---
+++ assets/javascript/app.js
@@ -0,0 +1,56 @@
+// window on load. so that nothing happens before everything is loaded //
+
+// initial array of objects. //
+
+// a variable for the timer //
+    // set the timer for a certain amount of time //
+    // a button to start the timer. //
+    // fix the timer to a div //
+
+// parent function for everything gameplay and the function that the timer will effect //
+
+    // a prevent default function, to keep the window from refreshing //
+
+    // the variables for the questions and the possible answers. //
+
+    //   //
+
+    // 1.  dynamically create divs and give them the same classes //
+            // make sure to give the divs there own classes, ids, names, etc. //
+            // make sure to give the <p> tags their own ID's so that each question can be attached easily to its own tag.//
+
+    // 2.  attach the questions to those tags. //
+
+    // 4. print to the <p> tags. //
+
+    // 5.  dynamically create the radio buttons and attach them to the form tag in the html with <p> tags //
+            // make sure to give all the buttons in the form classes, functions, names, etc. //
+            // make sure to give the buttons <p> tags id's as well. so that each set of possible answers can be attached to its own tag //
+
+    // 5.  attach the possible answers to the radio buttons <p> tags//
+
+    // 6.  print to the <p> tags //
+
+    // a check function to see if the answer is true or not //
+    
+    // an onclick event for the reset button and the start button //
+        // the start button //
+            // dynamically create this button. and make a function for it to work. //
+            // attach this button to the onclick event. //
+            // how does the button interact with the page? what does it do? and where do you put them? //
+            // attach the button to a variable for later use. //
+
+
+        // the reset button //
+            // dynamically create this button. and make a function for it to work. //
+            // attach this button to the onclick event. //
+            // how does the button interact with the page? what does it do? and where do you put them? //
+            // attach the button to a variable for later use. //
+
+
+    // call the original function //
+        // call this original function outside of the parent function. //
+
+    
+
+    
