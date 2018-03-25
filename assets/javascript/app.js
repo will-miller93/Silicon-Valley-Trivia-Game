32b27bab223558413ef3fbd4c65923e122b1c2c1
Will Miller <willmiller@Wills-MBP.attlocal.net>
2018-03-25 02:48:52 -0400

    big changes

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,56 +1,168 @@
 // window on load. so that nothing happens before everything is loaded //
+$(document).ready( function () {
 
 // initial array of objects. //
+var questions = [{
+    Question: "What is the name of the test used to measure the performance of lossless compression engines?",
+    choices: ["Size test", "Keirsey Test", "Weissman test", "Eysenck Test"],
+    rightanswer: 2
+}, {
+    Question: "what is the name that gilfoyle gives to his server?",
+    choices: ["Anton", "Lucifer", "Mini Me", "Nothing it's a server!"],
+    rightanswer: 0
+}, {
+    Question: "what is Pied Piper?",
+    choices: ["A Music Player", "A Search Engine", "Gilfoyles nickname","A Compression Engine"],
+    rightanswer: 3
+}, {
+    Question: "What is the name of the venture capital firm that funded Richards company?",
+    choices: ["Golden Gate Ventures", "Raviga", "GGV Capital", "Advanced Technology Ventures"],
+    rightanswer: 1
+}, {
+    Question: "What language does jared speak in his sleep?",
+    choices: ["Polish", "German", "Swedish", "French"],
+    rightanswer: 1
+}, {
+    Question: "What is Jareds real name?",
+    choices: ["Jared", "Richard", "Donald", "Gavin"],
+    rightanswer: 2
+}, {
+    Question: "Gilfoyle was an undocumented immagrant from which country?",
+    choices: ["Sweden", "Germany", "Russia", "Canada"],
+    rightanswer: 3
+}, {
+    Question: "What type of furniture was pied piper compared to in the promotional video?",
+    choices: ["a table", "a chair", "a door", "a stool"],
+    rightanswer: 0
+}, {
+    Question:  "who is Richards “girlfriend”?",
+    choices: ["Big Head", "His Phone", "His Inhaler", "His Computer"],
+    rightanswer: 3
+}, {
+    Question: "How much did Gavin belson initially offer for pied piper?",
+    choices: ["$1,000,000", "$600,000", "$10,000,000", "$500,000"],
+    rightanswer: 1
+}];
 
-// a variable for the timer //
-    // set the timer for a certain amount of time //
-    // a button to start the timer. //
-    // fix the timer to a div //
+// variables //
+var correctAnswers;
+var incorrectAnswers;
+var timer = 30;
+var timeOut;
+var startButton = $("#start-button");
+var submitButton = $("#submit-button").hide();
+var resetButton = $("#reset-button").hide();
+var radioButtons = $("#radio-group").hide();
+var questionText = $("#questionText").hide();
+var index = 1;
 
-// parent function for everything gameplay and the function that the timer will effect //
 
-    // a prevent default function, to keep the window from refreshing //
 
-    // the variables for the questions and the possible answers. //
 
-    //   //
+// function for the timer. //
+function countdownTimer() {
+    timer = 30;
+    clearInterval(timeOut);
+    timeOut = setInterval(decrement, 1000);
+}
 
-    // 1.  dynamically create divs and give them the same classes //
-            // make sure to give the divs there own classes, ids, names, etc. //
-            // make sure to give the <p> tags their own ID's so that each question can be attached easily to its own tag.//
+function decrement() {
+    timer--;
+    $(".timer").html(timer);
+        
+    if (timer === 0) {
+        stopTimer();
+        index++;
+        alert("Gotta be quicker!!");
+    }
 
-    // 2.  attach the questions to those tags. //
+}
 
-    // 4. print to the <p> tags. //
+function stopTimer() {
+    clearInterval(timeOut);
+}
 
-    // 5.  dynamically create the radio buttons and attach them to the form tag in the html with <p> tags //
-            // make sure to give all the buttons in the form classes, functions, names, etc. //
-            // make sure to give the buttons <p> tags id's as well. so that each set of possible answers can be attached to its own tag //
 
-    // 5.  attach the possible answers to the radio buttons <p> tags//
 
-    // 6.  print to the <p> tags //
-
-    // a check function to see if the answer is true or not //
+// function for the actual gameplay //
+function gamePlay() {
     
-    // an onclick event for the reset button and the start button //
-        // the start button //
-            // dynamically create this button. and make a function for it to work. //
-            // attach this button to the onclick event. //
-            // how does the button interact with the page? what does it do? and where do you put them? //
-            // attach the button to a variable for later use. //
-
-
-        // the reset button //
-            // dynamically create this button. and make a function for it to work. //
-            // attach this button to the onclick event. //
-            // how does the button interact with the page? what does it do? and where do you put them? //
-            // attach the button to a variable for later use. //
-
-
-    // call the original function //
-        // call this original function outside of the parent function. //
-
+    $("#questionText").html(questions[0].Question)
+    $("#posOne").html(questions[0].choices[0]);
+    $("#posTwo").html(questions[0].choices[1]);
+    $("#posThree").html(questions[0].choices[2]);
+    $("#posFour").html(questions[0].choices[3]);
     
+};
 
+function checkAnswers() {
+    if ($("#posThree") === questions[0].rightanswer) {
+        console.log("true");
+    }
+    // checks the answer depending on which radio button is selected.
+    // use an if statement to see which radio button is selected.
+    // check that against the question.answer
+
+};
+
+// on click events for the buttons. ///
+startButton.on("click", function() {
+    startButton.hide();
+    $(".timer").show();
+    questionText.show();
+    radioButtons.show();
+    submitButton.show();
+    countdownTimer();
+    gamePlay();
     
+    // needs to propogate the first question in the for loop, maybe call the submit button?
+    // call the countdownTimer function.
+    // calls the gamePlay function.
+
+});
+submitButton.on("click", function() {
+    // resetButton.show();
+    stopTimer();
+    
+    index++;
+    if (index < questions.length) {
+        $("#questionText").html(questions[index].Question);
+        $("#posOne").html(questions[index].choices[0]);
+        $("#posTwo").html(questions[index].choices[1]);
+        $("#posThree").html(questions[index].choices[2]);
+        $("#posFour").html(questions[index].choices[3]);
+        countdownTimer();
+    }    
+    
+    if( index === questions.length ) {
+        stopTimer();
+        questionText.hide();
+        radioButtons.hide();
+        submitButton.hide();
+        resetButton.show();
+        $(".timer").hide();
+        
+        // call the end screen function
+        
+    }
+    
+          
+});
+
+resetButton.on("click", function() {
+    questionText.hide();
+    resetButton.hide();
+    startButton.show();
+    // this button resets the entire game.
+    
+});
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////// 
+
+});
+
+ 
+
+
+ 
+
+  
